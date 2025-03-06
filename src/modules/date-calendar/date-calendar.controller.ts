8916d5c75e056efc945d704101444cd437031dd7
heosangbeom <luv_3_u@naver.com>
2025-03-06 20:23:27 +0900

    feat add get all date calendars

--- src/modules/date-calendar/date-calendar.controller.ts
+++ src/modules/date-calendar/date-calendar.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Inject, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, Inject, UseGuards, Get, Query, BadRequestException } from '@nestjs/common';
 import { DateCalendarService } from './date-calendar.service';
 import { CreateDateCalendarDto } from './dto/create-date-calendar.dto';
 import { DATE_CALENDAR_LOG_MESSAGES } from '../../constants/messages/date-calendar/api.message';
@@ -8,13 +8,15 @@ import { Logger } from 'winston';
 import { ApiResponseDto } from 'src/api/api.response.dto';
 import { HTTP_STATUS } from 'src/constants';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiCreatedResponse } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiQuery } from '@nestjs/swagger';
 import { ApiBody } from '@nestjs/swagger';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { ApiOperation } from '@nestjs/swagger';
 import { SWAGGER_CONSTANTS } from 'src/constants/common/swagger.constant';
 import { PrincipalDto } from 'src/modules/auth/dto/principal.dto';
 import { LoginUser } from 'src/core/decorators/login-user.decorator';
+import { GetAllDateCalendarsDto } from './dto/get-all-date-calendar.dto';
+import { DATE_CALENDAR_ERROR_MESSAGES } from 'src/constants/messages/date-calendar/error.message';
 @Controller('date-calendars')
 export class DateCalendarController {
     private logFormatter: AppLogFormatter;
@@ -44,6 +46,21 @@ export class DateCalendarController {
             DATE_CALENDAR_LOG_MESSAGES.SUCCESS.CREATE,
             response,
             HTTP_STATUS.SUCCESS.CREATED
+        );
+    }
+
+    @ApiOperation({ summary: DATE_CALENDAR_LOG_MESSAGES.API_CALLED.GET_ALL })
+    @ApiBearerAuth(SWAGGER_CONSTANTS.ACCESS_TOKEN)
+    @UseGuards(AuthGuard())
+    @Get()
+    async getAllDateCalendars(@LoginUser() principalDto: PrincipalDto, @Query() dto: GetAllDateCalendarsDto) {
+        const logPayload = this.logFormatter.format(DATE_CALENDAR_LOG_MESSAGES.API_CALLED.GET_ALL, { principalDto });
+        this.logger.log(logPayload);
+        const response = await this.dateCalendarService.getAllDateCalendars(principalDto.coupleId, dto);
+        return ApiResponseDto.success(
+            DATE_CALENDAR_LOG_MESSAGES.SUCCESS.GET_ALL,
+            response,
+            HTTP_STATUS.SUCCESS.OK
         );
     }
 }
--- src/modules/date-calendar/date-calendar.service.ts
+++ src/modules/date-calendar/date-calendar.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Between, Repository } from 'typeorm';
 import { DateCalendar } from './date-calendar.entity';
 import { CreateDateCalendarDto } from './dto/create-date-calendar.dto';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
@@ -8,6 +8,7 @@ import { Logger } from 'winston';
 import { AppLogFormatter } from '../../logger/log.formatter';
 import { DATE_CALENDAR_SERVICE_MESSAGES } from '../../constants/messages/date-calendar/service.message';
 import { DATE_CALENDAR_ERROR_MESSAGES } from '../../constants/messages/date-calendar/error.message';
+import { GetAllDateCalendarsDto } from './dto/get-all-date-calendar.dto';
 @Injectable()
 export class DateCalendarService {
     private logFormatter: AppLogFormatter;
@@ -33,5 +34,26 @@ export class DateCalendarService {
         return {
             id: dateCalendar.id,
         };
+    }
+
+    async getAllDateCalendars(coupleId?: number, dto?: GetAllDateCalendarsDto): Promise<DateCalendar[]> {
+        const logPayload = this.logFormatter.format(DATE_CALENDAR_SERVICE_MESSAGES.API_CALLED.GET_ALL, { dto });
+        this.logger.log(logPayload);
+        if(!coupleId) {
+            throw new BadRequestException(DATE_CALENDAR_ERROR_MESSAGES.ERROR.COUPLE_ID_REQUIRED);
+        }
+        if(!dto) {
+            throw new BadRequestException(DATE_CALENDAR_ERROR_MESSAGES.ERROR.DTO_REQUIRED);
+        }
+        // 월 시작일과 월 종료일 사이의 날짜 조회
+        const startDate = new Date(dto.year, dto.month - 1, 1);
+        const endDate = new Date(dto.year, dto.month, 0);
+        const dateCalendars = await this.dateCalendarRepository.find({
+            where: {
+                coupleId: coupleId,
+                date: Between(startDate, endDate),
+            },
+        });
+        return dateCalendars;
     }
 }
