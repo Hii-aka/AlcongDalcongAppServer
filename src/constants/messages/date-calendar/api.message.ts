fe457c67019f14f9e453a1f762dcbb107d42c9b5
heosangbeom <luv_3_u@naver.com>
2025-03-06 22:53:06 +0900

    feat 데이트 일정 상세 조회 기능 추가

--- src/constants/messages/date-calendar/api.message.ts
+++ src/constants/messages/date-calendar/api.message.ts
@@ -3,10 +3,12 @@ export const DATE_CALENDAR_LOG_MESSAGES = {
         CREATE: '날짜 캘린더 생성 요청',
         GET_ALL: '날짜 캘린더 조회 요청',
         GET_DATE_CALENDAR: '날짜 캘린더 조회 요청',
+        GET_DATE_CALENDAR_BY_ID: '날짜 캘린더 상세 조회 요청',
     },
     SUCCESS: {
         CREATE: '날짜 캘린더 생성 성공',
         GET_ALL: '날짜 캘린더 조회 성공',
         GET_DATE_CALENDAR: '날짜 캘린더 조회 성공',
+        GET_DATE_CALENDAR_BY_ID: '날짜 캘린더 상세 조회 성공',
     },
 };
